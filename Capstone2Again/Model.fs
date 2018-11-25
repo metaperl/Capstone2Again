95092d0949863be47befd6cc136e5e93b1841b0a
Terrence Brannon <metaperl@gmail.com>
2018-11-25 09:06:54 -0700

    unresolved symbols in program.

--- Capstone2Again/Model.fs
+++ Capstone2Again/Model.fs
@@ -1,10 +1,33 @@
 ﻿module Model
 
+open System.IO
+open System.IO
+open System.Net.Http
 
 type Customer = { name: string }
 
-type Acccount = 
+type Account = 
     { balance: int
       id: string
       customer: Customer }
+
+let deposit amount account =
+    {account with balance = account.balance + amount}
+
+
+
+
+
+// turn string into a path
+// create a directory path - https://docs.microsoft.com/en-us/dotnet/api/system.io.directory.createdirectory?view=netframework-4.7.2
+// get basename of a path  - https://docs.microsoft.com/en-us/previous-versions/windows/silverlight/dotnet-windows-silverlight/3x9wbdze(v=vs.95)
+// ensure directory exists?
+
+let createBaseDirectoryOf path =
+    GetDirectoryName path |> DirectoryInfo.CreateDirectory 
+
+let fileSystemAudit acct message =
+    let fileSystemPath = sprintf "C:\temp\learnfs\capstone2\%s\%s.txt" acct.customer.name acct.id
+    _ = createBaseDirectoryOf fileSystemPath
+    _ = File.WriteAllText fileSystemPath MessageProcessingHandler
 
--- Capstone2Again/Play.fsx
+++ Capstone2Again/Play.fsx
@@ -1,5 +1,9 @@
-﻿open Model
+﻿#load "Model.fs"
+open Model
 
-let b = { name: "bill" }
+let b = { name = "bill" }
+let ba = { balance = 10; id = "asdfasdfasdf"; customer = b}
 
+let c = deposit 45 ba
 
+createBaseDirectoryOf @"c:\temp\somedir\somefile.txt"
